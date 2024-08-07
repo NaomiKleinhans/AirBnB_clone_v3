bc4456fd9f91761cf20f95812bd47725484c6a47
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-07 21:45:54 +0200

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ Blueprint for API """
 from api.v1.views import (
-    places_amenities, users, amenities, cities, places_reviews, places, states, index
+    places_amenities,
+    users,
+    amenities,
+    cities,
+    places_reviews,
+    places,
+    states,
+    index
 )
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import views after the blueprint is created to avoid circular imports
