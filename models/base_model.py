651f075f4b4d2f30c4e9456c27f5a8786602331c
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-07 20:15:17 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 Contains class BaseModel
 """
 
-from datetime import datetime
+from datetime import datetime, timezone
 import models
 from os import getenv
 import sqlalchemy
@@ -23,8 +23,8 @@ class BaseModel:
     """The BaseModel class from which future classes will be derived"""
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
-        created_at = Column(DateTime, default=datetime.utcnow)
-        updated_at = Column(DateTime, default=datetime.utcnow)
+        created_at = Column(DateTime, default=datetime.now(timezone.utc))
+        updated_at = Column(DateTime, default=datetime.now(timezone.utc))
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -32,19 +32,21 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if kwargs.get("created_at", None) and isinstance(self.created_at, str):
+                self.created_at = datetime.strptime(
+                    kwargs["created_at"], time).replace(tzinfo=timezone.utc)
             else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+                self.created_at = datetime.now(timezone.utc)
+            if kwargs.get("updated_at", None) and isinstance(self.updated_at, str):
+                self.updated_at = datetime.strptime(
+                    kwargs["updated_at"], time).replace(tzinfo=timezone.utc)
             else:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.now(timezone.utc)
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.now(timezone.utc)
             self.updated_at = self.created_at
 
     def __str__(self):
