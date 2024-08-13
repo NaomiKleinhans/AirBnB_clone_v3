049e94be24a56372ca7af2075699f931b89daa28
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-13 22:12:13 +0200

    init views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,7 +7,7 @@ The blueprint is used to organize and register the various view modules
 that handle specific endpoints of the API.
 """
 
-from api.v1.views.places_amenities import *
+from api.v1.views import places_amenities
 from api.v1.views.places_reviews import *
 from api.v1.views.places import *
 from api.v1.views.users import *
@@ -17,4 +17,4 @@ from api.v1.views.states import *
 from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__)
