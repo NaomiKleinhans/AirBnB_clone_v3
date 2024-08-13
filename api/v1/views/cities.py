419ee27417860fad5cb86b042d80893a9778c01f
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-13 22:36:00 +0200

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -149,3 +149,8 @@ def put_city(city_id):
 
     storage.save()
     return jsonify(city.to_dict())
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})
