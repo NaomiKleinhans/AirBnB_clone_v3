ed420d0cadb3ccb179f3e93f17b7377b350a0d84
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-08-13 22:27:58 +0200

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,10 @@
-#!/usr/bin/python3
-"""API Blueprint for version 1 of the App.
-
-This module initializes the Flask blueprint for version 1 of the API.
-It sets the URL prefix for all routes under this version to '/api/v1'.
-The blueprint is used to organize and register the various view modules
-that handle specific endpoints of the API.
-"""
-
-from api.v1.views import cities
-from api.v1.views import users
-from api.v1.views import places
-from api.v1.views import places_amenities
 from flask import Blueprint
 
+# Create Blueprint
 app_views = Blueprint('app_views', __name__)
+
+# Import view modules here
+from api.v1.views import places_amenities
+from api.v1.views import places
+from api.v1.views import users
+from api.v1.views import cities
